544f31d132bde87ee7ce5233324fb80d0fc25808
Emiel Tasseel <emiel.tasseel@gmail.com>
2015-07-18 19:58:33 +0200

    Removed forwarding of compression packets. (Not needed)

--- src/main/java/org/quartzpowered/network/codec/PacketCodec.java
+++ src/main/java/org/quartzpowered/network/codec/PacketCodec.java
@@ -1,21 +1,21 @@
 /**
  * This file is a component of Quartz Powered, this license makes sure any work
  * associated with Quartz Powered, must follow the conditions of the license included.
- *
+ * <p>
  * The MIT License (MIT)
- *
+ * <p>
  * Copyright (c) 2015 Quartz Powered
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -48,13 +48,17 @@ import javax.inject.Inject;
 import java.util.List;
 
 public class PacketCodec extends ByteToMessageCodec<Packet> {
-    @Inject private Logger logger;
-    @Inject private SessionManager sessionManager;
-    @Inject private FactoryRegistry factoryRegistry;
+    @Inject
+    private Logger logger;
+    @Inject
+    private SessionManager sessionManager;
+    @Inject
+    private FactoryRegistry factoryRegistry;
 
     private final boolean clientSide;
 
-    @Inject private PacketCodec(@Assisted boolean clientSide) {
+    @Inject
+    private PacketCodec(@Assisted boolean clientSide) {
         this.clientSide = clientSide;
     }
 
@@ -142,11 +146,7 @@ public class PacketCodec extends ByteToMessageCodec<Packet> {
             logger.warn("Not all bytes read for {} - {} - {}", protocol, state, codec.getClass());
             buffer.skipBytes(buffer.readableBytes());
         }
-
-        if (packet instanceof CompressionPacket) {
-            ctx.fireChannelRead(packet);
-        } else {
-            out.add(packet);
-        }
+        
+        out.add(packet);
     }
 }
